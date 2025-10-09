c5fd3a3b20a40db898e3325b6ee55074800c3a25
Madhavi Putta <Madhavi Putta@LAPTOP-3C9HTLJT>
2025-10-09 21:54:15 +0530

    parameter

--- demotest/src/demotest/paramtest.java
+++ demotest/src/demotest/paramtest.java
@@ -1,16 +1,14 @@
 package demotest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class paramtest {
 
-	import static org.junit.jupiter.api.Assertions.assertTrue;
-	import org.junit.jupiter.params.ParameterizedTest;
-	import org.junit.jupiter.params.provider.ValueSource;
-
-	public class paramtest {
-
-	    @ParameterizedTest
-	    @ValueSource(strings = {"cali", "bali"})
-	    void endswith(String str) {
-	        assertTrue(str.endsWith("i"));
-	    }
-	}
+    @ParameterizedTest
+    @ValueSource(strings = {"cali", "bali"})
+    void endsWithI(String str) {
+        assertTrue(str.endsWith("i"));
+    }
+}
